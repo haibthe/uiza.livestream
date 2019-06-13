08ac81adbaa6a779f02ab8452cd2c06997c404ee
haibt <haibt@sendo.vn>
2019-06-13 14:34:15 +0700

    Init project

---
+++ uizalivestream/src/main/java/uizalivestream/interfaces/CameraCallback.java
@@ -0,0 +1,5 @@
+package uizalivestream.interfaces;
+
+public interface CameraCallback {
+    void onCameraChange(boolean isFrontCamera);
+}
---
+++ uizalivestream/src/main/java/uizalivestream/interfaces/UZLivestreamCallback.java
@@ -0,0 +1,30 @@
+package uizalivestream.interfaces;
+
+import uizalivestream.model.PresetLiveStreamingFeed;
+import uizalivestream.view.UZLivestream;
+import vn.uiza.restapi.uiza.model.v3.metadata.getdetailofmetadata.Data;
+
+public interface UZLivestreamCallback {
+    void onUICreate();
+
+    void onPermission(boolean areAllPermissionsGranted);
+
+    void onError(String reason);
+
+    void onGetDataSuccess(Data d, String mainUrl, boolean isTranscode,
+            PresetLiveStreamingFeed presetLiveStreamingFeed);
+
+    void onConnectionSuccessRtmp();
+
+    void onConnectionFailedRtmp(String reason);
+
+    void onDisconnectRtmp();
+
+    void onAuthErrorRtmp();
+
+    void onAuthSuccessRtmp();
+
+    void surfaceCreated();
+
+    void surfaceChanged(UZLivestream.StartPreview startPreview);
+}
